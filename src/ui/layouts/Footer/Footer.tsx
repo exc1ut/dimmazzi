5767ea6088482d25f5daa5e4fac6e57380ebee20
Hope1999 <umid19999@gmail.com>
2022-04-16 02:26:23 +0500

    add repo

---
+++ src/ui/layouts/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"; 
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars 
+export type FooterProps = {}
+
+export const Footer = (props:FooterProps) => { 
+	return (
+			<div>
+				Footer
+			</div>
+		); 
+};
